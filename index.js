4b7521dbe0e1a2ea413d4df815f0f4f3b5f52ae4
Ubuntu <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-03-14 23:54:02 +0000

    Part 1 a commit

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-var express = require('express')
-var app = express()
+var fs = require('fs')
 
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(requres, response) {
-response.readFileSync(__dirname + '/index.html')
-})
+fs.readFileSync(__dirname + '/index.html')
 
 var port = process.env.PORT || 8080
-
-app.listen(port, function() {
-	console.log("Node app is running at:" + port)
-})
 
