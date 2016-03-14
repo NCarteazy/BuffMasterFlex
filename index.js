89440a6a624730fb4189861404319491c111177a
Ubuntu <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-03-14 23:22:15 +0000

    small change -i

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(requres, response) {
+response.sendfile(__dirname + '/index.html')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+var port = process.env.PORT || 8080
+
+app.listen(port, function() {
+	console.log("Node app is running at:" + port)
 })
+
