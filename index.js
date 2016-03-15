5c47dabc47ece9ced2cc1e2ac1acd2622a2bad4c
Ubuntu <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-03-15 23:00:11 +0000

    start of part 2

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ var port = process.env.PORT || 8080
 
 var requestListener = function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
+	var buff = new Buffer(1024);	
 
-	fs.readFile(__dirname + '/index.html', 'utf8',  function (err, data) {
-         if (err) {
-                return console.log(err);
-        }
-        res.write(data)
+	fs.open(__dirname + '/index.html', 'r',  function (err, fd) {
+         	if (err) {
+                	return console.log(err);
+        	}
+        
+		fs.read(fd, buff, 0, buff.length, null, function (err, bytesRead, buffer) {
+		 	if (err) {
+				return console.log(err);
+			}
+			if (bytesRead > 0) {
+				res.write(buff.toString('utf8'));
+			}
+			}
+	
         res.end();
-        });
+	});
 }
 var server = http.createServer(requestListener);
 server.listen(port);
