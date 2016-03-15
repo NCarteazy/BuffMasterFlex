898532d0630aea48d292d9a5b16db2adf6d95f48
Ubuntu <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-03-15 22:22:00 +0000

    ???

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http')
 var port = process.env.PORT || 8080
 
 var requestListener = function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html');i
+	res.writeHead(200, {'Content-Type': 'text/html'});
 
 	fs.readFile(__dirname + '/index.html', 'utf8', function (err, data) {
         if (err) {
