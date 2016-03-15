b64cec102cbc3a74586ef20d5176dd53cf5e04e4
Ubuntu <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-03-15 20:44:55 +0000

    trying

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require('express')
 var app = express()
-var fs = require('fs')
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(requres, response) {
-fs.readFileSync(__dirname + '/index.html')
+response.sendfile(__dirname + '/index.html')
 })
 
 var port = process.env.PORT || 8080
