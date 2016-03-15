95a53e9d33bebbac60fa98bbfdf090af7e0d354b
Ubuntu <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-03-15 22:36:07 +0000

    FINISHED PART 1A ACTUALLY...

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ var port = process.env.PORT || 8080
 var requestListener = function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 
-	fs.readFile(__dirname + '/index.html', 'utf8', rfHandler); 
-
-
-var rfHandler = function (err, data) {
-	 if (err) {
+	fs.readFile(__dirname + '/index.html', 'utf8',  function (err, data) {
+         if (err) {
                 return console.log(err);
         }
         res.write(data)
         res.end();
-	};
+        };
+	); 
 }
 var server = http.createServer(requestListener);
 server.listen(port);
