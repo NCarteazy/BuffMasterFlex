cf0d0c7fdcacd6e65a9b5ae6be393ced04a13b68
Ubuntu <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-03-16 19:52:28 +0000

    first morning try p2 7 closer...

--- index.js
+++ index.js
@@ -5,33 +5,32 @@ var port = process.env.PORT || 8080;
 var requestListener = function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 
-	var buff = new Buffer(512);
- 	var loca = 0;	
 	fs.open(__dirname + '/index.html', 'r',  function (err, fd) {
          	if (err) {
                 	return console.log(err);
         	}
 		fs.fstat(fd, function (err, stats) { 
-			var size = stats.size;
-			console.log( "Loca: " + loca);
-			if(loca < size - 512) {
-				loca = loca + 512;
-				console.log(loca );
-				fs.read(fd, buff, 0, 512, loca, function( err, bytesRead, buffer) {
-					console.log("Bytesread: " + bytesRead);
-					res.write(buffer.toString('utf8', 0, 512));	
-				});
+			if (err) {
+				return console.log(err);
 			}
-			else {
-			var remain = size - loca;
-			fs.read(fd, buff, loca, remain, loca, function( err, bytesRead, buffer) {
-			res.write(buffer.toString('utf8', 0, remain));
-			res.end();
-			console.log("End Hit");
-			});  
+			var bsize = stats.size,
+			csize = 512;
+			buffer = new Buffer(bSize),
+			bread = 0;
+			
+			while ( bread < bsize ) {
+				if ((bread + csize) > bsize) {
+					csize = (bsize - bread);
+				}
+			fs.read(fd, buffer, bread, csize, bread);
+			bread = bread + csize;
 			}
+		console.log(buffer.toString('utf8', 0, bsize));
+		res.write(buffer.toString('utf8', 0, bsize));
+		res.end();
+		fs.close(fd);	
+		}
 		}); 
-		fs.close(fd); 
 	});
 };
 var server = http.createServer(requestListener);
