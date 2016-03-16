e4873de088b9467cfb854357454b25f1e898a27a
Ubuntu <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-03-16 00:31:54 +0000

    p2 11

--- index.js
+++ index.js
@@ -6,40 +6,26 @@ var port = process.env.PORT || 8080;
 var requestListener = function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 
-	var size = fs.stat(__dirname + '/index.html', function (err, stats) {
-		if (err) {
-			console.log(err);
-		}
-		return stats.size;
-	});
-	var local = 0;
-	
 	var buff = new Buffer(512);
 	
 	fs.open(__dirname + '/index.html', 'r',  function (err, fd) {
          	if (err) {
                 	return console.log(err);
         	}
-        	if (local <= size - 512) {
-		fs.read(fd, buff, local, 512, local, function (err, bytesRead, buffer) {
-		 	if (err) {
-				return console.log(err);
+		fs.fstat(fd, function (err, stats) { 
+			var size = stats.size;
+			var loca = 0;
+			console.log( "Size of file: " + size);
+			for (loca = 0; loca < size; ) {
+				loca = loca + buff.length;
+				fs.read(fd, buff, 0, buff.length, loca, function( err, bytesRead, buffer) {
+					res.write(buffer.toString('utf8', 0, bytesRead));	
+				});
 			}
-			console.log(buffer.toString('utf8'));
-			local = local + 512;
-		});}
-		else {
-		var diff = size - local;
-		fs.read(fd, buff, local, diff, local, function (err, bytesRead, buffer) {
-			if (err) {
-				return console.log(err);
-			}
-			res.write(buffer);
-		 
-		});
-		}
+		}); 
+		res.end();
+		fs.close(fd); 
 	});
-res.end();
 };
 var server = http.createServer(requestListener);
 server.listen(port);
